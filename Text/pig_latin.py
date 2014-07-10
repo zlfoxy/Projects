c1dc5dbd7fcc26d0a6747a4b072d8b0f722b2440
zlfoxy <zlfoxy@qq.com>
2014-07-10 23:10:08 +0800

    new

--- Text/pig_latin.py
+++ Text/pig_latin.py
@@ -10,3 +10,25 @@
     'egg'->'eggway'
     'inbox'->'inboxway'
     'eight'->'eightway'
+"""
+def pig_latin(str):
+    vowels = ['a','e','i','o','u']
+    if str[0:1] in vowels:
+        return str+'way'
+    elif str[0:2] == 'qu':
+        return str[2:]+'quay'
+    else:
+        for s in str:
+            if s in vowels:
+                return str[str.find(s):]+str[:str.find(s)]+'ay'
+  
+#Here are some test code.
+str1 = 'stupid'
+print pig_latin(str1)
+str2 = 'amstrong'
+print pig_latin(str2)
+str3 = 'screen'
+print pig_latin(str3)
+str4 = 'fly'
+print pig_latin(str4)
+#yes,when the word is 'fly' the code go wrong.'y' is a vowel,too ,in this situaiton.   
