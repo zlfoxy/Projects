4207217f6e026a7b26cd5dcd387aba436c7571f1
zlfoxy <zlfoxy@qq.com>
2014-07-10 21:15:09 +0800

    first commit for testing

---
+++ Text/pig_latin.py
@@ -0,0 +1,12 @@
+""" Rules
+    for words that begin with consonant sounds,the initial consonant
+    or consonant cluster is moved to the end of the word, and 'ay' is added,
+    as in the following examples:
+    'happy'->'appyhay'
+    'duck' ->'uckday'
+    'glove'->'oveglay'
+    For words that begin with vowel sounds or silent letter, you just add
+    'way' to the end.Examples are:
+    'egg'->'eggway'
+    'inbox'->'inboxway'
+    'eight'->'eightway'
---
+++ Text/reverse.py
@@ -0,0 +1,5 @@
+def reverse(str):
+    return str[::-1]
+
+str = raw_input("input a string:")
+print reverse(str)
